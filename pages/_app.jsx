334dc4f19640d8fbb5a78df4d9f777ad86e9641a
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2024-01-21 15:00:00 +0000

    refactor: edited page title and layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
-                <title>Animes Freitas</title>
+                <title>Animes Freitas 😺</title>
                 <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet" />
             </Head>
             <Component {...pageProps} />
