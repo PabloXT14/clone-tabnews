4f0aad663f39713d96bb07e7ec06137abc47ccd2
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2024-01-25 20:07:48 +0000

    refactor: cadd icon to the page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,8 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
-                <title>Animes Freitas 😺</title>
+                <title>Animes Freitas</title>
+                <link rel="icon" type="image/ico" href="cat-favicon.ico" />
                 <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet" />
             </Head>
             <Component {...pageProps} />
