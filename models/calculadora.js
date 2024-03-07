13ef0759aff4acf58e31d3eb5df85e5725aead72
Pablo Alan <pabloxt14@gmail.com>
2024-03-07 01:39:05 +0000

    docs: training creation of testes

--- models/calculadora.js
+++ models/calculadora.js
@@ -4,4 +4,33 @@ function sum(number1, number2) {
   return number1 + number2;
 }
 
-exports.sum = sum;
+function subtraction(number1, number2) {
+  if (typeof number1 !== "number" || typeof number2 !== "number") return "Erro";
+
+  return number1 - number2;
+}
+
+function multiplication(number1, number2) {
+  if (typeof number1 !== "number" || typeof number2 !== "number") return "Erro";
+
+  return number1 * number2;
+}
+
+function division(number1, number2) {
+  if (typeof number1 !== "number" || typeof number2 !== "number") return "Erro";
+
+  if (number2 === 0) return "Erro";
+
+  const result = number1 / number2;
+
+  if (Number.isInteger(result)) return result;
+
+  if (!Number.isInteger(result)) return Number(result.toFixed(2));
+}
+
+module.exports = {
+  sum,
+  subtraction,
+  multiplication,
+  division,
+};
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,25 +1,171 @@
 const calculadora = require("../models/calculadora");
 
-test("Somar 2 + 2 deveria retornar 4", () => {
-  const result = calculadora.sum(2, 2);
+describe("Testes de soma", () => {
+  test("Somar 2 + 2 deveria retornar 4", () => {
+    const result = calculadora.sum(2, 2);
 
-  expect(result).toBe(4);
+    expect(result).toBe(4);
+  });
+
+  test("Somar 5 + 100 deveria retornar 105", () => {
+    const result = calculadora.sum(5, 100);
+
+    expect(result).toBe(105);
+  });
+
+  test("Somar 'banana' + 100 deveria retornar 'Erro'", () => {
+    const result = calculadora.sum("banana", 100);
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Somar 5 + 'batata' deveria retornar 'Erro'", () => {
+    const result = calculadora.sum(5, "batata");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Somar 1 + '1' deveria retornar 'Erro'", () => {
+    const result = calculadora.sum(1, "1");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Somar -1 + -1 deveria retornar -2", () => {
+    const result = calculadora.sum(-1, -1);
+
+    expect(result).toBe(-2);
+  });
+
+  test("Somar 1.98 + 0.01 deveria retornar 1.99", () => {
+    const result = calculadora.sum(1.98, 0.01);
+
+    expect(result).toBe(1.99);
+  });
 });
 
-test("Somar 5 + 100 deveria retornar 105", () => {
-  const result = calculadora.sum(5, 100);
+describe("Testes de subtração", () => {
+  test("Subtrair 1 - 3 deveria retornar -2", () => {
+    const result = calculadora.subtraction(1, 3);
 
-  expect(result).toBe(105);
+    expect(result).toBe(-2);
+  });
+
+  test("Subtrair 100 - 5 deveria retornar 95", () => {
+    const result = calculadora.subtraction(100, 5);
+
+    expect(result).toBe(95);
+  });
+
+  test("Subtrair 'banana' - 10 deveria retornar 'Erro'", () => {
+    const result = calculadora.subtraction("banana", 10);
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Subtrair 10 - 'banana' deveria retornar 'Erro'", () => {
+    const result = calculadora.subtraction(10, "banana");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Subtrair 1 - '1' deveria retornar 'Erro'", () => {
+    const result = calculadora.subtraction(1, "1");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Subtrair (-1) - (-1) deveria retornar 0", () => {
+    const result = calculadora.subtraction(-1, -1);
+
+    expect(result).toBe(0);
+  });
+
+  test("Subtrair 10.99 - 1.98 deveria retornar 9.01", () => {
+    const result = calculadora.subtraction(10.99, 1.98);
+
+    expect(result).toBe(9.01);
+  });
 });
 
-test("Somar 'banana' + 100 deveria retornar 'Erro'", () => {
-  const result = calculadora.sum("banana", 100);
+describe("Testes de multiplicação", () => {
+  test("Multiplicar 10 * 7 deveria retornar 70", () => {
+    const result = calculadora.multiplication(10, 7);
 
-  expect(result).toBe("Erro");
+    expect(result).toBe(70);
+  });
+
+  test("Multiplicar 'banana' * 70 deveria retornar 'Erro'", () => {
+    const result = calculadora.multiplication("banana", 70);
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Multiplicar 70 * 'banana' deveria retornar 'Erro'", () => {
+    const result = calculadora.multiplication(70, "banana");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Multiplicar 70 * '1' deveria retornar 'Erro'", () => {
+    const result = calculadora.multiplication(70, "1");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Multiplicar -70 * -1 deveria retornar 70", () => {
+    const result = calculadora.multiplication(-70, -1);
+
+    expect(result).toBe(70);
+  });
+
+  test("Multiplicar 70.86 * 1.75 deveria retornar 124.005", () => {
+    const result = calculadora.multiplication(70.86, 1.75);
+
+    expect(result).toBe(124.005);
+  });
 });
 
-test("Somar 5 + 'batata' deveria retornar 'Erro'", () => {
-  const result = calculadora.sum(5, "batata");
+describe("Testes de divisão", () => {
+  test("Dividir 10 / 2 deveria retornar 5", () => {
+    const result = calculadora.division(10, 2);
 
-  expect(result).toBe("Erro");
+    expect(result).toBe(5);
+  });
+
+  test("Dividir 10 / 0 deveria retornar 'Erro'", () => {
+    const result = calculadora.division(10, 0);
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Dividir 0 / 10 deveria retornar 0", () => {
+    const result = calculadora.division(0, 10);
+
+    expect(result).toBe(0);
+  });
+
+  test("Dividir 'banana' / 10 deveria retornar 'Erro'", () => {
+    const result = calculadora.division("banana", 10);
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Dividir 10 / 'banana' deveria retornar 'Erro'", () => {
+    const result = calculadora.division(10, "banana");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Dividir 10 / '2' deveria retornar 'Erro'", () => {
+    const result = calculadora.division(10, "2");
+
+    expect(result).toBe("Erro");
+  });
+
+  test("Dividir 1 / 3 deveria retornar 0.33", () => {
+    const result = calculadora.division(1, 3);
+
+    expect(result).toBe(0.33);
+  });
 });
